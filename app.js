b8e8c3cbc6da3aaf8640eb68830aa6abffd258c8
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-03-21 15:21:03 +0530

    basic route

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the server side' });
+});
+
+app.post('/',(req,res) => {
+    res.send("You can send data to this route");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on ${port}`);
+});
