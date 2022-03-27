18aa1c7f62995cb70d5d5f0dc08e94fe0e11933f
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-03-27 15:34:27 +0530

    code refactored

--- app.js
+++ app.js
@@ -31,8 +31,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-//Server initiation
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on ${port}`);
-});
+module.exports = app;
