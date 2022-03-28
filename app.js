2c5bc90d66618893ee9928955599974a062ae459
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-03-28 15:22:11 +0530

    linting setup

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 //Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,33 @@
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'this route has not been implemented',
+    message: 'this route has not been implemented'
   });
 };
 
 exports.addUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'this route has not been implemented',
+    message: 'this route has not been implemented'
   });
 };
 
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'this route has not been implemented',
+    message: 'this route has not been implemented'
   });
 };
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'this route has not been implemented',
+    message: 'this route has not been implemented'
   });
 };
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'this route has not been implemented',
+    message: 'this route has not been implemented'
   });
 };
