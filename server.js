ec5093b8149154db79feab5e36fe11056820715f
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-04-11 15:34:16 +0530

    schema and model generation

--- server.js
+++ server.js
@@ -14,6 +14,24 @@ mongoose
   })
   .catch();
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: [true, 'Atour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 //Server initiation
 const port = process.env.PORT;
 app.listen(port, () => {
