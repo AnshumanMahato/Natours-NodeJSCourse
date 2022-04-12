be0b9e297e40f72b195d276cbbeb53e93ffa77f6
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-04-12 14:51:37 +0530

    updatet tours done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,13 +50,24 @@ exports.addTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success'
-    // data: {
-    //   tour,
-    // },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      error: err
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
