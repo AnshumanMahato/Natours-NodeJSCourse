2a5111ae8977ffcd24e902a5edc798a6f547b87c
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-05-18 22:14:53 +0530

    dev and production error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,40 @@
+const sendErrorDev = (err, res) => {
+  //In dev environment all errors are important
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //in PRODUCTION only errors related to client operations need to be sent to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    //For other errors we just log the error and send a generic message
+    console.error('ERROR 💥💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
