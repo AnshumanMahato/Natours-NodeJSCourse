410795eb2444d95d07ebd4d9418234b0807d08b6
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-07-02 10:52:13 +0530

    update user details

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filter = (obj, ...allowedKeys) => {
+  const newObj = {};
+  Object.keys(obj).forEach(key => {
+    if (allowedKeys.includes(key)) newObj[key] = obj[key];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -9,6 +18,33 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users
+    }
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //Check that user does not send password
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        'Password update is not allowed on this route. Use /updateMyPassword',
+        400
+      )
+    );
+  //Filter allowed fields
+  const filteredObj = filter(req.body, 'name', 'email');
+
+  //update user
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredObj, {
+    new: true,
+    runValidators: true
+  });
+
+  //send updated user
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser
     }
   });
 });
