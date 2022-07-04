e73b0c732ed9f8bf58287cce3b7d44f173233807
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-07-04 10:24:53 +0530

    nested review route

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -16,18 +16,15 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.addReview = catchAsync(async (req, res, next) => {
-  const review = {
-    ...req.body
-  };
+  if (!req.body.user) req.body.user = req.user._id;
+  if (!req.body.tour) req.body.tour = req.params.tourId;
 
-  review.user = req.user._id;
-
-  const savedReview = await Review.create(review);
+  const review = await Review.create(req.body);
 
   res.status(200).json({
     status: 'success',
     data: {
-      review: savedReview
+      review
     }
   });
 });
