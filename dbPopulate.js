e6dd3c9a588d82f3aacac6cbe157ae590c1def8b
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-07-04 15:47:09 +0530

    db loaded

--- dbPopulate.js
+++ dbPopulate.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const Tour = require('./models/tourModel');
+const User = require('./models/userModel');
+const Review = require('./models/reviewModel');
 require('dotenv').config({
   path: './config.env'
 });
@@ -21,11 +23,19 @@ mongoose
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/users.json`, 'utf-8')
+);
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/reviews.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -37,6 +47,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
