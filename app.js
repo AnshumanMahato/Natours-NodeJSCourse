8e7134c7fec510b39b924aae843c1f692c684e7e
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-07-06 14:30:37 +0530

    upto map

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
@@ -24,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(helmet());
+// app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -75,10 +76,8 @@ app.use(
 
 //Route handling with route method
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
