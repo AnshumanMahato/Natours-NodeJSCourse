a84131981e95ed3916a838868d16358b9c31f2a0
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-07-08 21:46:17 +0530

    stripe webhook implementattion

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,7 +45,7 @@ const createBookingCheckout = async session => {
   console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
 };
 
