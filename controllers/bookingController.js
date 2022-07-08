f5a59a4cd3807e2645a75a44663203a775760313
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-07-08 21:27:21 +0530

    stripe webhook implementattion

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,10 +45,11 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
+  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
 
   let event;
@@ -62,8 +63,10 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+  if (event.type === 'checkout.session.completed') {
+    console.log('event got');
+    await createBookingCheckout(event.data.object);
+  }
 
   res.status(200).json({ received: true });
 };
